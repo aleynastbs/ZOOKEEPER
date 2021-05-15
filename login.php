01d29596dfef838189645409b7557ee89abba86c
LAPTOP-2NE4OHFB\hp <aleynasutbas@hotmail.com>
2021-05-15 19:12:07 +0300

    login changes

--- login.php
+++ login.php
@@ -32,28 +32,34 @@
         ?>
        
        <div class="wrapper fadeInDown">
-        <div id="formContent">
-            <!-- Tabs Titles -->
-
+        <div id="formContent"><p>&nbsp;</p>
             <!-- Icon -->
             <div class="fadeIn first">
-            <img src="https://upload.wikimedia.org/wikipedia/commons/3/39/Italian_traffic_signs_-_icona_zoo.svg" id="icon" alt="User Icon" />
+            <img src="https://upload.wikimedia.org/wikipedia/commons/3/39/Italian_traffic_signs_-_icona_zoo.svg" id="icon" alt="User Icon" /><p>&nbsp;</p>
             </div>
 
             <!-- Login Form -->
-            <form>
-            <input type="text" id="login" class="fadeIn second" name="login" placeholder="login">
-            <input type="text" id="password" class="fadeIn third" name="login" placeholder="password">
-            <input type="submit" class="fadeIn fourth" value="Log In">
+            <form method="get">
+            <input type="text" id="login" class="fadeIn second" name="username" placeholder="login" required/>
+            <input type="password" id="password" class="fadeIn third" name="password" placeholder="password" required/>
+            <input type="submit" class="fadeIn fourth" name= "login" value="Log In">
             </form>
-
-            <!-- Remind Passowrd -->
-            <div id="formFooter">
-            <a class="underlineHover" href="#">Forgot Password?</a>
-            </div>
 
             </div>
         </div>
+
+        <?php
+            if ($_SERVER['REQUEST_METHOD'] == 'GET') {
+                if(isset($_GET['login'])){
+                  if(isset($GET['username']) && isset($GET['username'])){
+                    $username = $GET['username'];  
+                    $sql = "SELECT * FROM User WHERE User.username = '$username'";
+                    $user_info = mysqli_query($mysqli,$sql);
+                    echo $user_info->fetch_assoc()["user_id"];
+                  }
+                }
+            }
+        ?>
         <script src="https://code.jquery.com/jquery-3.6.0.js" integrity="sha256-H+K7U5CnXl1h5ywQfKtSj8PCmoN9aaq30gDh27Xc0jk=" crossorigin="anonymous"></script>
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-p34f1UUtsS3wqzfto5wAAmdvj+osOnFyQFpp4Ua3gs/ZVWx6oOypYoCJhGGScy+8" crossorigin="anonymous"></script>
         
